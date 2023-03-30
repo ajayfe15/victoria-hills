85bb298c9efe16b616459c0d5798bce9c4d5b42a
ajay <ajay@firsteconomy.com>
2023-03-30 15:41:34 +0530

    overview section created

---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+	<title>Victoria Hills</title>
+	<meta content="" name="description">
+	<meta content="" name="keywords">
+
+	<!-- Favicons -->
+	<link href="assets/img/favicon.png" rel="icon">
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;800&display=swap" rel="stylesheet">
+
+	<!-- Vendor CSS Files -->
+	<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+	<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+	<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+	<!-- Template Main CSS File -->
+	<link href="assets/css/style.css" rel="stylesheet">
+	<link href="assets/css/responsive.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+	<!-- ======= Header ======= -->
+	<header id="header" class="fixed-top ">
+		<div class="container d-flex align-items-center">
+
+			<a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+
+			<nav id="navbar" class="navbar">
+				<ul>
+					<li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+					<li><a class="nav-link scrollto" href="#overview">Overview</a></li>
+					<li><a class="nav-link scrollto" href="#plots">Plots</a></li>
+					<li><a class="nav-link scrollto" href="#gallery">Gallery</a></li>
+					<li><a class="nav-link scrollto" href="#villas">Villas</a></li>
+					<li><a class="nav-link scrollto" href="#connectivity">Connectivity</a></li>
+					<li><a class="getstarted scrollto" href="#contactUs">Contact Us</a></li>
+				</ul>
+				<i class="bi bi-list mobile-nav-toggle"></i>
+			</nav><!-- .navbar -->
+
+		</div>
+	</header><!-- End Header -->
+
+	<main id="main">
+		<section class="videoSection" id="#hero">
+			<video autoplay="" muted="" loop="" id="myVideo">
+				<source src="assets/img/video.mp4" type="video/mp4">
+			</video>
+		</section>
+
+		<section class="overview" id="overview">
+			<div class="container">
+				<div class="commonHeading d-flex flex-column justify-content-center align-items-center">
+					<h2>VICTORIA HILLS</h2>
+					<p>TIMELESS LIFESTYLE AWAITS YOU</p>
+					<img src="assets/img/vicotoia-hills.png" alt="" class="img-fluid">
+				</div>
+
+				<div class="overviewContent">
+					<div class="row justify-content-center">
+						<div class="col-lg-10">
+							<p class="m-0 text-center">What if we offered you an address that combines the peace and calm of Karjat with the eternal beauty of the Victorian Era? <b>Welcome to Victoria Hills</b>, a vast expanse of land set amidst serene surroundings and lofty hills. Victoria Hills evokes a euphoric feeling as you witness the confluence of the timeless Victorian era and the timeless beauty of Karjat. Every element, piece of architecture and structure is finely crafted with the Victorian era in mind. Here you don't have to live with a timestamp as Victoria Hills gives you a chance to live a Timeless Lifestyle.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="container-fluid overviewImgBoxWrapper">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="overviewImgBox">
+							<img src="assets/img/overview-img/01.jpg" alt="" class="img-fluid">
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="overviewImgBox">
+							<img src="assets/img/overview-img/02.jpg" alt="" class="img-fluid">
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="overviewImgBox">
+							<img src="assets/img/overview-img/03.jpg" alt="" class="img-fluid">
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="overviewImgBox">
+							<img src="assets/img/overview-img/04.jpg" alt="" class="img-fluid">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="plots creamBg" id="plots">
+			<div class="commonHeading d-flex flex-column justify-content-center align-items-center">
+				<h2>VICTORIA HILLS</h2>
+				<p>TIMELESS LIFESTYLE AWAITS YOU</p>
+				<img src="assets/img/vicotoia-hills.png" alt="" class="img-fluid">
+			</div>
+		</section>
+
+		<section class="gallery" id="gallery"></section>
+
+		<section class="villas" id="villas"></section>
+
+		<section class="connectivity" id="connectivity"></section>
+
+		<section class="contactUs" id="contactUs"></section>
+	</main><!-- End #main -->
+
+	<!-- ======= Footer ======= -->
+	<footer id="footer">
+		<div class="container footer-top">
+			<div class="row justify-content-center">
+				<div class="col-md-9">
+					<div class="row justify-content-between align-items-center">
+						<div class="col-md-6">
+							<img src="https://via.placeholder.com/150x90" class="img-fluid" alt="">
+						</div>
+						<div class="col-md-6">
+							<div class="siteAdrressBlock">
+								<p><strong>Site Address - </strong> Shingdhol, Karjat</p>
+								<p><strong>Office Address - </strong> G-4 Ground Floor, A-Wing, Dev Corpora, Opp. To Cadbury Junction, Thane (W)-400601</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container footer-bottom">
+			<div class="copyright d-flex">
+				<div class="left">
+					Copyright &copy; 2023. All Rights Reserved by Victoria Hills.
+				</div>
+				<div class="right">
+					Designed & Developed by <a href="https://firsteconomy.com/" target="_blank">First Economy</a>
+				</div>
+
+			</div>
+		</div>
+	</footer><!-- End Footer -->
+
+	<div class="sidebar-contact">
+		<div class="toggle"></div>
+		<h2>Enquire Now</h2>
+		<form autocomplete="off" class="lead_form" method="POST">
+			<div class="rows">
+				<div class="col-12">
+					<input autocomplete="off" type="text" class="form-control capture" placeholder="NAME*" name="name2" id="name2" required="">
+					<div class="error_msg error" id="name2_error">Please Enter Your Name</div>
+				</div>
+				<div class="col-12">
+					<input autocomplete="off" type="tel" class="form-control capture" placeholder="MOBILE NO*" name="mobile2" id="mobile2" required="" onkeypress="return alpha(event)" maxlength="10">
+					<div class="error_msg error" id="mobile2_error">Please Enter Your Mobile Number</div>
+				</div>
+				<div class="col-12">
+					<input autocomplete="off" type="email" class="form-control capture" placeholder="EMAIL ID*" name="email2" id="email2" required="">
+					<div class="error_msg error" id="email2_error">Please Enter Your Email</div>
+				</div>
+				<div class="col-12">
+					<textarea name="form-control" id="" rows="3" placeholder="YOUR MESSAGE"></textarea>
+				</div>
+
+				<div class="text-center">
+					<button class="btn btn-default" type="submit" id="form2">
+						<span>Submit</span>
+					</button>
+				</div>
+			</div>
+		</form>
+	</div>
+
+	<!--pop Model -->
+	<div id="popup-modalForm" class="modal fade" aria-modal="true" role="dialog">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<button type="button" class="btn-close close" data-bs-dismiss="modal" aria-label="Close"></button>
+				<div class="modal-body">
+					<h2>Enquire Now</h2>
+					<form autocomplete="off" class="lead_form" method="POST">
+						<div class="col-12">
+							<input autocomplete="off" type="text" class="form-control capture" placeholder="NAME*" name="name1" id="name1" required="">
+							<div class="error_msg error" id="name1_error">Please Enter Your Name</div>
+						</div>
+						<div class="col-12">
+							<input autocomplete="off" type="tel" class="form-control capture" placeholder="MOBILE NO*" name="mobile1" id="mobile1" required="" onkeypress="return alpha(event)" maxlength="10">
+							<div class="error_msg error" id="mobile1_error">Please Enter Your Mobile Number</div>
+						</div>
+						<div class="col-12">
+							<input autocomplete="off" type="email" class="form-control capture" placeholder="EMAIL ID*" name="email1" id="email1" required="">
+							<div class="error_msg error" id="email1_error">Please Enter Your Email</div>
+						</div>
+						<div class="text-center">
+							<button class="btn btn-default" type="submit" id="form1"> <span>Submit</span> </button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="preloader"></div>
+	<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+	<!-- Vendor JS Files -->
+	<script src="assets/js/jquery-3.6.0.min.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+
+	<!-- Template Main JS File -->
+	<script src="assets/js/main.js"></script>
+	<script src="assets/js/custom.js"></script>
+
+</body>
+
+</html>
