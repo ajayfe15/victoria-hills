897f6d4e8a1cc3526d10ee0de36e6af69a40fa7e
ajay <ajay@firsteconomy.com>
2023-03-31 17:53:21 +0530

    config file changes done

--- admin/config.php
+++ admin/config.php
@@ -2,6 +2,6 @@
 date_default_timezone_set("Asia/Kolkata");
 $mysql_hostname = "localhost";
 $mysql_user = "root";
-$mysql_password = "root";
+$mysql_password = "fe_db_admin";
 $mysql_database = "victoria_hills";
 $db = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Database Connection Problem a");mysqli_select_db($db, $mysql_database) or die("Database Connection Problem b");
